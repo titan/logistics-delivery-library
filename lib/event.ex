b4219756ff018a1a1df0c5205920066e6ebce250
Titan <titan@kachebang.com>
2016-04-07 21:11:31 +0800

    Add created_at to event

--- lib/event.ex
+++ lib/event.ex
@@ -19,8 +19,9 @@ defmodule Delivery.Event do
   | source      | string    | 发货地址     |
   | destination | string    | 收货地址     |
   | image       | image     | 照片信息     |
+  | created_at  | timestamp | 运单创建时间 |
 
   """
-  defstruct type: 0, oid: nil, occurred_at: 0, did: nil, no: nil, eid: nil, consigner: nil, consignee: nil, item: nil, quantity: 0, description: nil, source: nil, destination: nil, image: nil
-  @type t :: %Delivery.Event{type: non_neg_integer, oid: String.t, occurred_at: non_neg_integer, did: String.t, no: String.t, eid: String.t, consigner: Delivery.Person.t, consignee: Delivery.Person.t, item: String.t, quantity: non_neg_integer, description: String.t, source: String.t, destination: String.t, image: Delivery.Image.t}
+  defstruct type: 0, oid: nil, occurred_at: 0, did: nil, no: nil, eid: nil, consigner: nil, consignee: nil, item: nil, quantity: 0, description: nil, source: nil, destination: nil, image: nil, created_at: 0
+  @type t :: %Delivery.Event{type: non_neg_integer, oid: String.t, occurred_at: non_neg_integer, did: String.t, no: String.t, eid: String.t, consigner: Delivery.Person.t, consignee: Delivery.Person.t, item: String.t, quantity: non_neg_integer, description: String.t, source: String.t, destination: String.t, image: Delivery.Image.t, created_at: 0}
 end
